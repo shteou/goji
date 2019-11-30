8b716aba0d3dda0b8567817bf91e5931abc980ae
Stewart Platt <shteou@gmail.com>
2019-11-30 23:01:23 +0000

    Apply queued jobs to kubernetes

--- GojiApp/commands/process.py
+++ GojiApp/commands/process.py
@@ -5,15 +5,26 @@ from GojiApp.jobs import *
 # Applies all queued jobs and transitions to processing/failed
 # as required
 def process(jobs):
+  print(f"Processing jobs: {jobs}")
+  print("Checking for duplicate jobs")
   duplicates = duplicate_jobs()
   if bool(duplicates):
     print("Error, found the following duplicate jobs: ", duplicates)
     sys.exit(1)
+  print("No duplicates found")
 
   for job in jobs:
-    success = apply_job(job)
-    if success == True:
-      move_job(job, "queued", "processing")
-    else:
-      move_job(job, "queued", "failed")
+    try:
+      print(f"Processing {job}")
+      if apply_job(job):
+        move_job(job, "queued", "processing")
+        print(f"Processing {job} succeeded")
+      else:
+        move_job(job, "queued", "failed")
+        print(f"Processing {job} failed")
 
+    except Exception as e:
+      print(f"Encountered an unexpected exception whilst processing {job}")
+      print(e)
+
+  print("Finished processing jobs")
